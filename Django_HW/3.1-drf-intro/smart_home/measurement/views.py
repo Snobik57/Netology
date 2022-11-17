5c61a97f17dded2729875a3a9a981802efb2405c
Snobik57 <snobik57@gmail.com>
2022-11-17 09:10:44 +0600

    change smart-home

--- Django_HW/3.1-drf-intro/smart_home/measurement/views.py
+++ Django_HW/3.1-drf-intro/smart_home/measurement/views.py
@@ -1,6 +1,3 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
 
 
@@ -12,36 +9,13 @@ class SensorsView(ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
-    def perform_create(self, serializer):
-        specification = self.request.data.get('specification')
-        designation = self.request.data.get('designation')
-
-        return serializer.save(specification=specification,
-                               designation=designation)
-
 
 class SingleSensorView(RetrieveUpdateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
-    def perform_update(self, serializer):
-        specification = self.request.data.get('specification')
-        designation = self.request.data.get('designation')
-
-        return serializer.save(specification=specification,
-                               designation=designation)
-
 
 class MeasurementCreateView(CreateAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
-
-    def perform_create(self, serializer):
-        sensor = self.request.data.get('sensor_id')
-        sensor_id = Sensor.objects.get(id=sensor)
-        temperature = self.request.data.get('temperature')
-        photo = self.request.data.get('photo')
-
-        return serializer.save(sensor_id=sensor_id,
-                               temperature=temperature,
-                               photo=photo)
+    
