0266a46a2958717e806abb5c93dc3ff76a06e9a4
Snobik57 <snobik57@gmail.com>
2022-12-18 20:55:15 +0600

    ADD: hw_docker

--- Django_HW/3.3-permissions/api_with_restrictions/advertisements/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from advertisements.views import AdvertisementViewSet
-
-router = DefaultRouter()
-router.register('advertisements', AdvertisementViewSet)
-
-urlpatterns = router.urls
